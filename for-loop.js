659b6011533bc8960a890c566ce7b4ed4ff217b8
Henry T <tran_ht@hotmail.com>
2017-03-17 15:02:17 +0000

    Initial commit with all my work so far

---
+++ for-loop.js
@@ -0,0 +1,9 @@
+var total = 0
+var limit = 10
+
+     for (var i = 0; i < 10; i++) {
+       // log the numbers 0 through 9
+         total += i;
+
+     }
+      console.log(total)
---
+++ looping-through-arrays.js
@@ -0,0 +1,5 @@
+var pets = ['cat', 'dog', 'rat']
+for (var i) {
+  pets[i] = pets[i] + 's'
+}
+console.log(pets)
